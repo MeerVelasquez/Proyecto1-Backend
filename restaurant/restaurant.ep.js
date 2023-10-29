f96e4bcc7c08f867aae6bb4cb8188a378525e426
Eliana Del Mar Velásquez Rodriguez <103125856+MeerVelasquez@users.noreply.github.com>
2023-10-28 22:33:09 -0500

    restaurante ep 1.0

--- restaurant/restaurant.ep.js
+++ restaurant/restaurant.ep.js
@@ -41,6 +41,60 @@ db.once("open", async () => {
 
   //Read (Cantidad): Read (cantidad) El endpoint retorna los datos de los restaurantes que correspondana la categoría proveída y/o su nombre se asemeje a la búsqueda.
 
+  app.get('/restaurantes', async (req, res) => {
+    try{
+      const { category, name } = req.query;
+      const filter = {};
+  
+      if (category) {
+        filter.category = category;
+      }
+
+      if (name) {
+        filter.name = { $regex: name, $options: 'i' }; 
+      }
+      const restaurants = await Restaurant.find(filter);
+      if (restaurants.length === 0) {
+        return res.status(404).json({ message: 'No se encontraron restaurantes' });
+      }
+      res.status(200).json(restaurants);   
+    }catch(error){
+      res.status(500).json({ message: 'Error al buscar restaurantes' });
+    }});
+  
+    //Update El endpoint modifica los datos del restaurante que corresponde a la id proveída, usando los datos proveídos.
+
+    app.put('/restaurantes/:id', async (req, res) => {
+      const restaurantId = req.params.id;
+      const restaurantData = req.body;
+      try {
+        const restaurant = await Restaurant.findByIdAndUpdate(restaurantId, updateData, { new: true });
+    
+        if (!restaurant) {
+          return res.status(404).json({ message: 'Restaurante no encontrado' });
+        }
+    
+        res.status(200).json(restaurant);
+      } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar el restaurante' });
+      }
+
+    });
+
+    //Delete El endpoint “inhabilita” un restaurante que corresponde a la id proveída.
+
+    app.delete('/restaurantes/:id', async (req, res) => {
+      try{
+          const restaurant = await Restaurant.findByIdAndUpdate(req.params.id, { active: false }, { new: true });
+          if (!restaurant) {
+              return res.status(404).json({ message: 'Restaurante no encontrado' });
+            }
+            res.status(200).json(restaurant);
+      }catch(error){
+          res.status(500).json({ message: 'Error al eliminar el restaurante' });
+      }});
+
+
  
 
 });
