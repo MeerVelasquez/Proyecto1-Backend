cae6ebdb352bbae61bd17e98a3aafac7d9f3c9a9
Eliana Del Mar Velásquez Rodriguez <103125856+MeerVelasquez@users.noreply.github.com>
2023-11-05 17:43:29 -0500

    db change

--- src/db.js
+++ src/db.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/bdCP", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Error de conexión a la base de datos:"));
+(async () => {
+    try {
+        const db = await mongoose.connect("mongodb://localhost:27017/bdCP", {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log(`Connection to database ${db.connection.name} successful`)
 
-// export default db;
+        db.connection.on('error', err => {
+            console.log(`Database connection error: ${err}`);
+        });
+
+        db.connection.on('disconnected', () => {
+            console.log('Database disconnected');
+        });
+    } catch(err) {
+        console.log('Couldn\'t connect to database', err);
+    }
+})();
+//export default db;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import './db.js';
+// import './db.js';
+import mongoose from "mongoose";
 import userRouter from './routes/user.ep.js';
 import productRouter from './routes/product.ep.js';
 import restaurantRouter from './routes/restaurant.ep.js';
@@ -7,17 +8,22 @@ import restaurantRouter from './routes/restaurant.ep.js';
 
 const app = express();
 const port = 3000;
-app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('Bienvenido a PseudoRappi')
-});
+const db = await mongoose.connect("mongodb://localhost:3000/bdCP", {useNewUrlParser: true, useUnifiedTopology: true});
 
+app.use(express.json());
 app.use(userRouter);
 app.use(productRouter);
 app.use(restaurantRouter);
 // app.use(orderRouter);
 
 
-app.listen(port, () => {
+db.listen(port, () => {
+
     console.log(`Example app listening at http://localhost:${port}`)
+    app.get('/', (req, res) => {
+        res.send('Bienvenido a PseudoRappi')
+    });
+    
   })
+
+
